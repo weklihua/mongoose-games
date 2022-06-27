32839b0e9f4e0a81da8a64defa03bc043abd7ee0
weklihua <wek.lihua@gmail.com>
2022-06-26 20:29:53 -0700

    register app

--- config/database.js
+++ config/database.js
@@ -1,3 +1,16 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/games');
+mongoose.connect(process.env.DATABASE_URL)
+
+// mongoose.connect('mongodb://127.0.0.1:27017/games');
+// mongoose.connect(process.env.DATABASE_URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true
+//   });
+
+const db = mongoose.connection
+
+db.on('connected', function() {
+    console.log(`Connected to MongoDB at ${db.host}:${db.port}`)
+})
