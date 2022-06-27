756d049020211022e1b38ca590c382f33ff100ae
weklihua <wek.lihua@gmail.com>
2022-06-27 00:54:25 -0700

    serializeUser and deserializeUser

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const session = require('express-session')
+const passport = require('passport');
 
 
 require('dotenv').config();
 require('./config/database')
+require('./config/passport');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -29,6 +32,10 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
