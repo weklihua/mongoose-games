22125103348b53a3400e2c627faad942a99e942c
weklihua <wek.lihua@gmail.com>
2022-06-28 21:40:03 -0700

    reach mvp

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const reviewsCtrl = require('../controllers/reviews')
+const isLoggedIn = require('../config/auth');
+
 
 router.post('/games/:id/reviews', reviewsCtrl.create)
-router.delete('/reviews/:id', reviewsCtrl.delete)
-router.get('/reviews/:id/edit', reviewsCtrl.edit)
-router.put('/reviews/:id', reviewsCtrl.update)
+router.delete('/reviews/:id', isLoggedIn, reviewsCtrl.delete)
+router.get('/reviews/:id/edit', isLoggedIn, reviewsCtrl.edit)
+router.put('/reviews/:id', isLoggedIn, reviewsCtrl.update)
 
 module.exports = router;
