c603a832ec46d3dae93c8852837da3a3b577ed7e
weklihua <wek.lihua@gmail.com>
2022-06-28 21:33:10 -0700

    add isLoggedIn

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -20,11 +20,7 @@ module.exports = {
 
   }
 
-  // controllers/reviews.js
-
-// Include the next parameter - used for error handling in the catch
 function deleteReview(req, res, next) {
-  // Note the cool "dot" syntax to query on the property of a subdoc
   Game.findOne({'reviews._id': req.params.id, 'reviews.user': req.user._id}).then(function(game) {
     // Rogue user!
     if (!game) return res.redirect('/games');
@@ -41,7 +37,19 @@ function deleteReview(req, res, next) {
     });
   });
 }
+// async function deleteReview(req, res, next) {
+//   try{
+//     const game = await game.findOne({'reviews._id' : req.params.id, 'reviews.user': req.user._id})
+//     // console.log(game)
+//     if (!game) return res.redirect('/games')
+//     game.reviews.remove(req.params.id)
+//     await game.save()
+//     res.redirect(`/games/${game._id}`)
 
+//   }catch(err){
+//     return next(err)
+//   }
+// }
 // async function deleteReview(req, res, next) {
 //         const game =await Game.findOne({'reviews._id' : req.params.id})
 //         // console.log(game)
@@ -63,7 +71,19 @@ function edit(req, res) {
     });
   });
 }
-  
+// function edit(req, res, next) {
+//   Game.findOne({'reviews._id': req.params.id, 'reviews.user': req.user._id}).then(function(game) {
+//     // Rogue user!
+//     if (!game) return res.redirect('/games');
+//     // Remove the review using the remove method available on Mongoose arrays
+//     const review = game.reviews.id(req.params.id);
+//     res.render('games/edit', {
+//       title: 'Edit Game',
+//       review
+//     })
+//   });
+// }
+
 // async function edit(req, res) {
 
 //     const game =await Game.findOne({'reviews._id' : req.params.id})
@@ -76,7 +96,7 @@ function edit(req, res) {
 //   }
 
 function update(req, res) {
-  Game.findOne({'reviews._id': req.params.id}, function(err, game) {
+  Game.findOne({'reviews._id': req.params.id, 'reviews.user': req.user._id}, function(err, game) {
 
     const reviewSubdoc = game.reviews.id(req.params.id);
     // Ensure that the review was created by the logged in user
--- routes/games.js
+++ routes/games.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const gamesCtrl = require('../controllers/games');
+const isLoggedIn = require('../config/auth');
+
 	
 // prefix: /games
 router.get('/', gamesCtrl.index)
-router.get('/new', gamesCtrl.new);
-router.post('/', gamesCtrl.create)
+router.get('/new', isLoggedIn, gamesCtrl.new);
+router.post('/', isLoggedIn, gamesCtrl.create)
 router.get('/:id', gamesCtrl.show)
 	
 module.exports = router;
