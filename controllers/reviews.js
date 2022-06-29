7c263a8b524399ea9d79974bbc6e0545c7bd8721
weklihua <wek.lihua@gmail.com>
2022-06-29 16:39:31 -0700

    sort updated reviews

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -21,6 +21,21 @@ module.exports = {
 
   }
 
+  // function create(req, res) {
+  //   console.log(req.body)
+  //   Game.findById(req.params.id, function(err, game){
+  //     req.body.user = req.user._id;
+  //     req.body.userName = req.user.name;
+  //     req.body.userAvatar = req.user.avatar;
+
+  //       game.reviews.push(req.body)
+  //       game.save(function(err) {
+  //           res.redirect(`/games/${game._id}`)
+  //       })
+  //   })
+
+  // }
+
 function deleteReview(req, res, next) {
   Game.findOne({'reviews._id': req.params.id, 'reviews.user': req.user._id}).then(function(game) {
     // Rogue user!
@@ -60,6 +75,19 @@ function deleteReview(req, res, next) {
 //         res.redirect(`/games/${game._id}`)
 
 //       }
+
+// function edit(req, res) {
+//   Game.findOne({'reviews._id': req.params.id}).sort('-updatedAt')
+//   .exec(function(err, game) {
+
+//     const review = game.reviews.id(req.params.id);
+//     console.log(review)
+//     res.render('games/edit', {
+//       title: 'Edit Game',
+//       review
+//     });
+//   });
+// }
 
 function edit(req, res) {
   Game.findOne({'reviews._id': req.params.id}, function(err, game) {
